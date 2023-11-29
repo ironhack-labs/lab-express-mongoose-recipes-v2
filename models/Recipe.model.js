842652c35ba038aeac8c197e04a809212bcdde73
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-29 17:41:58 +0100

    creating Recipe model

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,37 @@
-// Your code here ...
+const recipeSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingridients: {
+    type: [String],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  isAchived: {
+    type: Boolean,
+    default: false,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
