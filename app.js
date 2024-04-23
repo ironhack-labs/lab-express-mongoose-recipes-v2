4a33994918f539d5260b7f37e1f4302d8e35556b
Fabio Niglio <fabio_niglio@hotmail.com>
2024-04-23 14:43:00 +0200

    get recipes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -8,44 +10,47 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
-
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+    app.listen(3000, () => console.log("My first app listening on port 3000!"));
+  })
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
-
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipe", async (req, res) => {});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", async (req, res) => {
+  const recipes = await Recipe.find();
+  res.json({ message: "All Good", data: recipes });
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
 
-
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
-
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
