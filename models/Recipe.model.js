d1e54b1b4c8279a04b98c0749d8872c7992193c4
Rodolfo Bittkau <tyrmund@gmail.com>
2024-05-22 14:59:02 +0200

    lab completed

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,37 @@
-// Your code here ...
+const { model, Schema } = require("mongoose")
+
+const recipeSchema = new Schema(
+    {
+        title: {
+            type: String
+        },
+        instructions: {
+            type: String
+        },
+        level: {
+            type: String,
+            enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+        },
+        ingredients: {
+            type: [String]
+        },
+        image: {
+            type: String,
+            default: "https://images.media-allrecipes.com/images/75131.jpg"
+        },
+        duration: {
+            type: Number,
+            min: 0
+        },
+        isArchived: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    })
+
+const Recipe = model('Recipe', recipeSchema)
+
+module.exports = Recipe
