f4a828d884fc680e19e429612373ce9d93352ac3
Marta Merchan <martamerchan16@gmail.com>
2024-08-13 16:30:14 +0200

    server and endpoints ok

--- app.js
+++ app.js
@@ -3,47 +3,74 @@ const logger = require("morgan");
 
 const app = express();
 
-// MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
 
-// Iteration 1 - Connect to MongoDB
-// DATABASE CONNECTION
+require('./db')
+
+const Recipe = require('./models/Recipe.model')
+
+
+app.post('/api/recipes', (req, res) => {
+
+    const { title, instructions, level, ingredients, image, duration, isArchived } = req.body
+
+    Recipe
+        .create({ title, instructions, level, ingredients, image, duration, isArchived })
+        .then(recipe => res.sendStatus(201))
+        .catch(err => res.status(500).json({ code: 500, message: 'Server error', details: err }))
+})
+
+
+app.get('/api/recipes', (req, res) => {
+
+    Recipe
+        .find()
+        .then(recipes => res.json(recipes))
+        .catch(err => res.status(500).json({ code: 500, message: 'Server error', details: err }))
+})
+
+
+app.get('/api/recipes/:recipeId', (req, res) => {
+
+    const { recipeId } = req.params
+    const { title, instructions, level, ingredients, image, duration, isArchived } = req.body
+
+    Recipe
+        .findById(recipeId)
+        .then(recipe => res.json(recipe))
+        .catch(err => res.status(500).json({ code: 500, message: 'Server error', details: err }))
+})
+
+
+app.put('/api/recipes/:recipeId', (req, res) => {
+
+    const { recipeId } = req.params
+    const { title, instructions, level, ingredients, image, duration, isArchived } = req.body
+
+    Recipe
+        .findByIdAndUpdate(recipeId, { title, instructions, level, ingredients, image, duration, isArchived })
+        .then(recipe => res.sendStatus(200))
+        .catch(err => res.status(500).json({ code: 500, message: 'Server error', details: err }))
+
+})
 
 
 
-// ROUTES
-//  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
-});
+app.delete('/api/recipes/:recipeId', (req, res) => {
+
+    const { recipeId } = req.params
+
+    Recipe
+        .findByIdAndDelete(recipeId)
+        .then(recipe => res.sendStatus(200))
+        .catch(err => res.status(500).json({ code: 500, message: 'Server error', details: err }))
+})
 
 
-//  Iteration 3 - Create a Recipe route
-//  POST  /recipes route
-
-
-//  Iteration 4 - Get All Recipes
-//  GET  /recipes route
-
-
-//  Iteration 5 - Get a Single Recipe
-//  GET  /recipes/:id route
-
-
-//  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
-
-
-//  Iteration 7 - Delete a Single Recipe
-//  DELETE  /recipes/:id route
-
-
-
-// Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
+app.listen(5005, () => console.log('My first app listening on port 5005!'));
 
 
 
