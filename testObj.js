f64f7b7e928b10b67eb49d179b9def9d53e88dcd
Baptiste Desharbes <desharbesbaptiste@hotmail.fr>
2024-12-04 15:15:32 +0100

    all iterations done

---
+++ testObj.js
@@ -0,0 +1,7 @@
+const testRecipe = {
+  title: "mashed potatoes",
+  instructions: "Boil water and put pataties in it, then mash the potatoes",
+  level: "Easy Peasy",
+  ingredients: ["Potatoes", "butter"],
+  duration: 10,
+};
