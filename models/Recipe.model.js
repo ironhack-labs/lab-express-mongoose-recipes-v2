e4d1400168b55d8483d8990d2ff182cfd2882563
Mariah <alinemariah@hotmail.com>
2025-01-14 21:42:24 +0100

    donce

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,11 @@
 // Your code here ...
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema ({
+    title: {type: String, required: true, unique: true},
+    instructions: {type: String, required: true},
+    level: {}
+})
+
